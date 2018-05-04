a82dc214e2eb954256a89c78c9ecc848d114d809
RealityMachina <tomclancy1995@gmail.com>
2018-05-03 20:05:11 -0400

    Rebased and expanded mod to use the base of mpstark's mod for Harmony: https://github.com/Mpstark/LessPilotInjuries
    Contains same features, should be platform agnostic.

--- source/mainClass.cs
+++ source/mainClass.cs
@@ -5,46 +5,79 @@ using System.Text;
 using System.Reflection;
 using BattleTech;
 using Harmony;
+using UnityEngine;
 
+
+//code taken and modified from: https://github.com/Mpstark/LessPilotInjuries
 namespace LessHeadInjuries
 {
 
-
-    public class Mech_HarmonyPatch
+    public static class LessHeadInjuries
     {
-   
-        
-        public static void HarmonyPatch()
+        public static float ArmorHeadHitIgnoreDamageBelow = 10;
+        public static float StructHeadHitIgnoreDamageBelow = 5;
+        public static HashSet<Pilot> IgnoreNextHeadHit = new HashSet<Pilot>();
+
+        public static void Init()
         {
-            HarmonyInstance harmony = HarmonyInstance.Create("Battletech.realitymachina.Mech_HarmonyPatch");
+            var harmony = HarmonyInstance.Create("Battletech.realitymachina.LessHeadInjuries");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-
-            // find the ApplyHeadStructureEffects method of the class BattleTech.Mech
-            MethodInfo targetmethod = AccessTools.Method(typeof(BattleTech.Mech), "ApplyHeadStructureEffects");
-
-            // find the static method to call before (i.e. Prefix) the targetmethod
-            HarmonyMethod prefixmethod = new HarmonyMethod(typeof(LessHeadInjuries.Mech_HarmonyPatch).GetMethod("ApplyHeadStructureEffects_Prefix"));
-
-            // patch the targetmethod, by calling prefixmethod before it runs, with no postfixmethod (i.e. null)
-            harmony.Patch(targetmethod, prefixmethod, null);
-
         }
 
-        //a prefix method like this can be set to return void or bool
-        //use void if you want it to just alter something before the original method
-        //here, we use bool since we want to override the original method
-        public static bool ApplyHeadStructureEffects_Prefix(Mech __instance, ref ChassisLocations location, ref LocationDamageLevel oldDamageLevel, ref LocationDamageLevel newDamageLevel, ref string sourceID, ref int stackItemUID)
+        public static void Reset()
         {
-            if (newDamageLevel == oldDamageLevel || newDamageLevel != LocationDamageLevel.Destroyed)
-                return true;
+            IgnoreNextHeadHit = new HashSet<Pilot>();
+        }
+    }
 
-            __instance.pilot.SetNeedsInjury(InjuryReason.HeadHit);
+    [HarmonyPatch(typeof(BattleTech.GameInstance), "LaunchContract", new Type[] { typeof(Contract), typeof(string) })]
+    public static class BattleTech_GameInstance_LaunchContract_Patch
+    {
+        static void Postfix()
+        {
+            // reset on new contracts
+            LessHeadInjuries.Reset();
+        }
+    }
 
-            __instance.pilot.LethalInjurePilot(sourceID, stackItemUID, true, "Cockpit Destroyed");
-            __instance.Combat.MessageCenter.PublishMessage((MessageCenterMessage)new AddSequenceToStackMessage((IStackSequence)new ShowActorInfoSequence((ICombatant)__instance, "PILOT: LETHAL DAMAGE!", FloatieMessage.MessageNature.PilotInjury, true)));
+    [HarmonyPatch(typeof(BattleTech.Mech), "DamageLocation")]
+    public static class BattleTech_Mech_DamageLocation_Patch
+    {
+        static void Prefix(Mech __instance, int originalHitLoc, WeaponHitInfo hitInfo, ArmorLocation aLoc, Weapon weapon, float totalDamage, int hitIndex, AttackImpactQuality impactQuality)
+        {
+            if (aLoc == ArmorLocation.Head)
+            {
+                //copied from main script: we do some quick calculation of damage to see if it's an armor hit or an structure hit
+                float currentArmor = __instance.GetCurrentArmor(aLoc);
+                float remainingDamage = Mathf.Min(totalDamage, currentArmor);
+
+                if (remainingDamage <= 0 && totalDamage < LessHeadInjuries.ArmorHeadHitIgnoreDamageBelow)
+                {
+                    //remainign damage less or equal to zero mean no structure penetration. Treat as an armour hit.
+                    LessHeadInjuries.IgnoreNextHeadHit.Add(__instance.pilot);
+                }
+                else if (remainingDamage > 0 && totalDamage < LessHeadInjuries.StructHeadHitIgnoreDamageBelow)
+                {
+                    LessHeadInjuries.IgnoreNextHeadHit.Add(__instance.pilot);
+                }
+
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleTech.Pilot), "SetNeedsInjury")]
+    public static class BattleTech_Pilot_SetNeedsInjury_Patch
+    {
+        static bool Prefix(Pilot __instance, InjuryReason reason)
+        {
+            if (reason == InjuryReason.HeadHit && LessHeadInjuries.IgnoreNextHeadHit.Contains(__instance))
+            {
+                LessHeadInjuries.IgnoreNextHeadHit.Remove(__instance);
+                return false;
+            }
 
             return true;
         }
-
     }
+
 }
